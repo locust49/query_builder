5c995db57cea39f3d85e2b5402b0f302696669b5
locust49 <slyazid@student.1337.ma>
2022-11-14 11:22:17 +0100

    chore: add logging config

---
+++ config/const.py
@@ -0,0 +1,1 @@
+logFileName = "query-builder"
---
+++ config/loggingConfig.py
@@ -0,0 +1,36 @@
+import logging
+from config.const import logFileName
+
+
+class Formatter(logging.Formatter):
+
+    default_format = "%(asctime)s\t[%(levelname)-5.5s]:\t%(message)s"
+    error_format = '\t--> Occured in "%(pathname)s":%(lineno)s'
+
+    def __init__(self, fmt=None):
+        if fmt is None:
+            fmt = self.default_format
+        super().__init__(fmt, style="%")
+
+    def format(self, record):
+        # Save the original format when instantiated
+        format_original = self._style._fmt
+
+        if record.levelno >= logging.ERROR:
+            self._style._fmt += self.error_format
+
+        # Calls the original formatter class to format
+        result = logging.Formatter.format(self, record)
+
+        # Restore the original format
+        self._style._fmt = format_original
+        return result
+
+
+logFormatter = Formatter()
+rootLogger = logging.getLogger()
+
+fileHandler = logging.FileHandler("{0}/{1}.log".format(".", logFileName))
+fileHandler.setFormatter(logFormatter)
+rootLogger.addHandler(fileHandler)
+rootLogger.setLevel(logging.INFO)
